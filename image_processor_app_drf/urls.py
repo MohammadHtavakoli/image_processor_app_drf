e02a9d903357bd86ca161fb8594c972de177137c
Mohammad Tavakoli <mohammadh.tavakoli81@gmail.com>
2025-02-12 15:08:44 +0330

    Update urls.py

--- image_processor_app_drf/urls.py
+++ image_processor_app_drf/urls.py
@@ -14,9 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Image Processor API',
+        default_version='v1',
+        description='API for processing images',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='mohammadh.tavakoli81@gmail.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    # path('', include('api.urls')),  # This will include the index view
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
